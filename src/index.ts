52cdfd4706991fb04262b45f02c164f74d5431c1
hi0711 <admin@hi0711.xyz>
2023-03-20 16:47:53 +0900

    puppeteer の出力結果をファイルに渡す (#9)
    * [WIP] puppeteer の出力結果をファイルに渡す
    
    * docs: ✏️  コメント追加
    
    * feat: 🎸 処理結果を json ファイルに書き込み
    
    ✅ Closes: #6
    
    * feat: 🎸 5分ごとに書き込むように調整
    
    ✅ Closes: #6
    
    * package: 📦 momentjs を導入
    
    ✅ Closes: #6
    
    * feat: 🎸 ファイル保存時にファイル名に日時を入れる
    
    ✅ Closes: #6

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
 import puppeteer from 'puppeteer';
+import fs from 'fs';
+import moment from 'moment';
 
 // スクレイピングするための関数
 const scrape = async (query: string) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  // URL 定義
   await page.goto(`https://www.google.com/search?q=${query}`);
+  // 日時取得
+  const date: string = moment().format('YYYYMMDDHHmmss');
 
+  // タイトルと URL を取得
   const result = await page.$$eval('.tF2Cxc', (elements) => {
     return elements.map((element) => {
       return {
@@ -15,7 +21,15 @@ const scrape = async (query: string) => {
     });
   });
 
-  console.log(result);
+  // 5分ごとに日時をファイル名に入れて保存
+  setInterval(() => {
+    fs.writeFile(`result-${date}.json`, JSON.stringify(result), (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }, 300000);
+
   await browser.close();
 }
 
