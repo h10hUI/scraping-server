b64797c38a6a653d9c870a6a470b8566c775c7da
hiroshi_satoh <admin@hi0711.xyz>
2023-06-28 16:57:22 +0900

    refactor: 💡 ダブルクォーテーションをシングルクォーテーションに変更

--- src/middleware/logging.ts
+++ src/middleware/logging.ts
@@ -1,10 +1,10 @@
-import morgan from "morgan";
-import fs from "fs";
-import path from "path";
-import winston from "winston";
-import { Express } from "express";
+import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
+import winston from 'winston';
+import { Express } from 'express';
 
-const logDir = path.join(__dirname, "../../logs");
+const logDir = path.join(__dirname, '../../logs');
 
 export const setupLogging = (app: Express) => {
   // logs ディレクトリがなければ作成
@@ -14,33 +14,33 @@ export const setupLogging = (app: Express) => {
 
   // アクセスログを出力するファイルの指定
   const accessLogStream = fs.createWriteStream(
-    path.join(logDir, "access.log"),
-    { flags: "a" }
+    path.join(logDir, 'access.log'),
+    { flags: 'a' }
   );
 
   // アクセスログを出力する
-  app.use(morgan("combined", { stream: accessLogStream }));
+  app.use(morgan('combined', { stream: accessLogStream }));
 };
 
 // winston の設定
 export const logger = winston.createLogger({
-  level: "info",
+  level: 'info',
   format: winston.format.json(),
   transports: [
     // error レベルのログを出力する
     new winston.transports.File({
-      filename: path.join(logDir, "error.log"),
-      level: "error",
+      filename: path.join(logDir, 'error.log'),
+      level: 'error',
     }),
     // info レベルのログを出力する
     new winston.transports.File({
-      filename: path.join(logDir, "combined.log")
+      filename: path.join(logDir, 'combined.log')
     }),
   ],
 });
 
 // 開発環境の場合は console にも出力する
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
   logger.add(
     new winston.transports.Console({
       format: winston.format.simple(),
